41e8be7d922ea6ee62355d200cd9b989fa97be61
vinicius <viniciusbellini@gmail.com>
2022-11-18 15:10:39 -0300

    Datepicker

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,16 +1,29 @@
 import './styles.css'
 import NotificationButton from "../NotificationButton"
 
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+
 function SalesCard() {
-    return(
+    return (
         <div className="dsmeta-card">
             <h2 className="dsmeta-sales-title">Vendas</h2>
             <div>
                 <div className="dsmeta-form-control-container">
-                    <input className="dsmeta-form-control" type="text" />
+                    <DatePicker
+                        selected={new Date()}
+                        onChange={(date: Date) => { }}
+                        className="dsmeta-form-control"
+                        dateFormat="dd/MM/yyyy"
+                    />
                 </div>
                 <div className="dsmeta-form-control-container">
-                    <input className="dsmeta-form-control" type="text" />
+                    <DatePicker
+                        selected={new Date()}
+                        onChange={(date: Date) => { }}
+                        className="dsmeta-form-control"
+                        dateFormat="dd/MM/yyyy"
+                    />
                 </div>
             </div>
 
@@ -49,9 +62,9 @@ function SalesCard() {
                             <td className="show992">11</td>
                             <td>R$ 55300.00</td>
                             <td>
-                            <div className="dsmeta-red-btn-container">
-                                <NotificationButton />
-                            </div>
+                                <div className="dsmeta-red-btn-container">
+                                    <NotificationButton />
+                                </div>
                             </td>
                         </tr>
                         <tr>
@@ -72,7 +85,6 @@ function SalesCard() {
             </div>
         </div>
     )
-  }
-  
-  export default SalesCard
-  
+}
+
+export default SalesCard
