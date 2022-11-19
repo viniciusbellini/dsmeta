00abe97d1de1aba9b08a8482f050c18496847e33
vinicius <viniciusbellini@gmail.com>
2022-11-19 10:45:37 -0300

    Api test

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -2,6 +2,8 @@ package com.devsuperior.dsmeta.repositories;
 
 import com.devsuperior.dsmeta.entities.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 }
